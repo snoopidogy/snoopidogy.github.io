753ccf1c895b10ba291436ce939102515490c856
snoopidogy <nqkoii90@gmail.com>
2025-06-16 21:47:45 +0300

    updated mac 2025 full working
    updated mac 2025 full working

---
+++ mac.bat
@@ -0,0 +1,41 @@
+@ECHO OFF
+SETLOCAL ENABLEDELAYEDEXPANSION
+SETLOCAL ENABLEEXTENSIONS
+
+:: Generate and implement a random MAC address that starts with 02-
+FOR /F "tokens=1" %%a IN ('wmic nic where physicaladapter^=true get deviceid ^| findstr [0-9]') DO (
+    CALL :MAC
+    FOR %%b IN (0 00 000) DO (
+        REG QUERY HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Class\{4D36E972-E325-11CE-BFC1-08002bE10318}\%%b%%a >NUL 2>NUL && REG ADD HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Class\{4D36E972-E325-11CE-BFC1-08002bE10318}\%%b%%a /v NetworkAddress /t REG_SZ /d !MAC!  /f >NUL 2>NUL
+    )
+)
+
+:: Disable power saving mode for network adapters
+FOR /F "tokens=1" %%a IN ('wmic nic where physicaladapter^=true get deviceid ^| findstr [0-9]') DO (
+    FOR %%b IN (0 00 000) DO (
+        REG QUERY HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Class\{4D36E972-E325-11CE-BFC1-08002bE10318}\%%b%%a >NUL 2>NUL && REG ADD HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Class\{4D36E972-E325-11CE-BFC1-08002bE10318}\%%b%%a /v PnPCapabilities /t REG_DWORD /d 24 /f >NUL 2>NUL
+    )
+)
+
+:: Reset NIC adapters so the new MAC address is implemented and power saving mode is disabled.
+FOR /F "tokens=2 delims=, skip=2" %%a IN ('"wmic nic where (netconnectionid like '%%') get netconnectionid,netconnectionstatus /format:csv"') DO (
+    netsh interface set interface name="%%a" disable >NUL 2>NUL
+    netsh interface set interface name="%%a" enable >NUL 2>NUL
+)
+
+GOTO :EOF
+
+:MAC
+:: Generates a semi-random MAC address starting with 02-
+SET MAC=02
+SET COUNT=1
+SET GEN=ABCDEF0123456789
+
+:MACLOOP
+SET /A COUNT+=1
+SET RND=%random%
+SET /A RND=RND%%16
+SET RNDGEN=!GEN:~%RND%,1!
+SET MAC=!MAC!!RNDGEN!
+
+IF !COUNT! LEQ 11 GOTO MACLOOP
